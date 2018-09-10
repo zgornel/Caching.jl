a749d7be699818d7c3ffe6fcc14fb3152bbe407b
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-09-10 10:25:34 +0200

    Changed @assert to @test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,20 +11,20 @@ end
 
 
 foo_c1 = LRUCaching.MemoryCache(foo)
-@assert typeof(foo_c1) <: LRUCaching.AbstractCache
-@assert typeof(foo_c1) <: LRUCaching.MemoryCache
-@assert foo(number1) == foo_c1(number1)
+@test typeof(foo_c1) <: LRUCaching.AbstractCache
+@test typeof(foo_c1) <: LRUCaching.MemoryCache
+@test foo(number1) == foo_c1(number1)
 
 foo_m1 = @memcache foo
-@assert typeof(foo_m1) <: LRUCaching.MemoryCache
-@assert foo(number1) == foo_m1(number1)
+@test typeof(foo_m1) <: LRUCaching.MemoryCache
+@test foo(number1) == foo_m1(number1)
 
 
 function bar(x; y=1)
 	return x+y
 end
 bar_c1 = LRUCaching.MemoryCache(bar)
-@assert typeof(bar_c1) <: LRUCaching.AbstractCache
-@assert typeof(bar_c1) <: LRUCaching.MemoryCache
-@assert bar(number1) == bar_c1(number1)
-@assert bar(number1; y=number2) == bar_c1(number1; y=number2)
+@test typeof(bar_c1) <: LRUCaching.AbstractCache
+@test typeof(bar_c1) <: LRUCaching.MemoryCache
+@test bar(number1) == bar_c1(number1)
+@test bar(number1; y=number2) == bar_c1(number1; y=number2)
